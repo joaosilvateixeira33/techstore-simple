bdef641890cf6508980ddbb08e70a6fd5840e22e
João Silva <joaoteixeirasilva33@gmail.com>
2024-12-01 18:36:21 -0300

    refactor: added 404, home in views page

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const ecommerce = require('./router/ecommerce'); 
 
 const app = express();
 const port = 5000;
+
 const hbs = exphbs.create({
-    partialsDir: ['views/partials'] 
+    partialsDir: ['views/partials']
 });
 
 app.use(express.static('public'));
 app.engine('handlebars', hbs.engine);
-app.use('views engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
+
+app.use('/', ecommerce);
+
+app.use(function(req, res){
+    res.status(404).render('404', { noHeader: true, noFooter: true });
+})
 app.listen(port, () =>{
     console.log(`Server on in http://localhost:${port}`);
 })
