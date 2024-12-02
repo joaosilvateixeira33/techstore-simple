a3bce94410af000ed28022be81065e83687dc420
João Silva <joaoteixeirasilva33@gmail.com>
2024-12-02 20:46:26 -0300

    refactor: added images and styles

--- data/db.js
+++ data/db.js
@@ -1,7 +1,7 @@
 const products = [
     {id: 1, image: "/images/notbook-1.png", name: 'Notbook Acer', price: 1000.00},
     {id: 2, image: "/images/monitor-1.png", name: 'Monitor Beta', price: 10000.00},
-    {id: 3, image: "/images/pc-gamer.png", name: 'PC Gamer Nova tech', price: 2000.00},
+    {id: 3, image: "/images/pc-gamer-1.png", name: 'PC Gamer Nova tech', price: 2000.00},
 ]
 
 module.exports = products;
