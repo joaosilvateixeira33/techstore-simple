c73b4a74b878cc41e405ed4c29c226d5761f34e2
João Silva <joaoteixeirasilva33@gmail.com>
2024-12-02 20:18:28 -0300

    refactor: structure complete

--- router/ecommerce.js
+++ router/ecommerce.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const products = require('../data/db');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.render('home')
+    res.render('home', {products});
+})
+
+router.get('/product/:id', (req, res)=>{
+    const id = req.params.id;
+    const product = products.find(p => p.id === Number(id));
+    if(product){
+        res.render('product', {product});
+    }else{
+        res.status(404).render('404', { noHeader: true, noFooter: true });
+    }
 })
 
 module.exports = router;
