ba4bcfe64da2643a651afddfc0daa0726d77f901
João Silva <joaoteixeirasilva33@gmail.com>
2024-12-02 21:36:57 -0300

    styled: Added styles in card items

--- data/db.js
+++ data/db.js
@@ -2,6 +2,8 @@ const products = [
     {id: 1, image: "/images/notbook-1.png", name: 'Notbook Acer', price: 1000.00},
     {id: 2, image: "/images/monitor-1.png", name: 'Monitor Beta', price: 10000.00},
     {id: 3, image: "/images/pc-gamer-1.png", name: 'PC Gamer Nova tech', price: 2000.00},
+    {id: 3, image: "/images/charlie-v6-1.png", name: 'Charlie V6', price: 10000.00},
+    {id: 3, image: "/images/zulu-v6-1.png", name: 'Zulu v6', price: 5000.00},
 ]
 
 module.exports = products;
